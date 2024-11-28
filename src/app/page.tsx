ff305c50651b79b93ab8f0ffd4d092899efc575e
Jinny You <jinny@lottiefiles.com>
2024-11-28 23:44:00 +0900

    🔧 Udpate metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "MotionDoc - The Future of Interactive Documents",
+  description: "MotionDoc is a revolutionary documentation file format that brings interactivity to documents",
   // other metadata
 };
 
